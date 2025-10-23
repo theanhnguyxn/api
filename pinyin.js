6f87a3d5d63683097ee382ead23eb9f8a0980d2a
Nguyễn Thế Anh <143569393+theanhnguyxn@users.noreply.github.com>
2025-10-23 22:28:58 +0700

    Delete pinyin.js

--- pinyin.js
+++
@@ -1,7 +0,0 @@
-import { pinyin } from "pinyin-pro";
-
-export default function handler(req, res) {
-  const text = req.query.p || "";
-  const result = pinyin(text, { toneType: "mark" });
-  res.status(200).json({ pinyin: result });
-}
